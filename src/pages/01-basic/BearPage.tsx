ae67d322ac1a502d96685a42995927b513ea24f5
LostoVayne <moises.barrera.dev@gmail.com>
2023-11-14 23:54:20 -0300

    agregado el store y los metodos de zustand

--- src/pages/01-basic/BearPage.tsx
+++ src/pages/01-basic/BearPage.tsx
@@ -1,3 +1,4 @@
+import { useShallow } from "zustand/react/shallow";
 import { WhiteCard } from "../../components";
 import { useBearStore } from "../../stores";
 
@@ -12,6 +13,7 @@ export const BearPage = () => {
                 <BlackBears />
                 <PolarBears />
                 <PandaBears />
+                <BearDisplay />
             </div>
         </>
     );
@@ -64,6 +66,28 @@ export const PandaBears = () => {
                 <span className="text-3xl mx-2 lg:mx-10"> {pandaBears} </span>
                 <button onClick={() => increasePandaBears(-1)}>-1</button>
             </div>
+        </WhiteCard>
+    );
+};
+
+export const BearDisplay = () => {
+    const bears = useBearStore(useShallow((state) => state.bears));
+    const doNothing = useBearStore((state) => state.doNothing);
+    const addBear = useBearStore((state) => state.addBear);
+    const clearBears = useBearStore((state) => state.clearBears);
+
+    return (
+        <WhiteCard>
+            <h1>Osos</h1>
+            <button onClick={doNothing}>Do nothing</button>
+            <button className="mt-2" onClick={addBear}>
+                Add bear
+            </button>
+            <button className="mt-2" onClick={clearBears}>
+                Clear bears
+            </button>
+
+            <pre>{JSON.stringify(bears, null, 2)}</pre>
         </WhiteCard>
     );
 };
--- src/stores/bears/bears.store.ts
+++ src/stores/bears/bears.store.ts
@@ -1,20 +1,49 @@
 import { create } from "zustand";
 
+interface Bears {
+    id: number;
+    name: string;
+}
+
 interface BearState {
     blackBears: number;
     polarBears: number;
     pandaBears: number;
+    bears: Bears[];
     increaseBlackBears: (by: number) => void;
     increasePolarBears: (by: number) => void;
     increasePandaBears: (by: number) => void;
+
+    doNothing: () => void;
+    addBear: () => void;
+    clearBears: () => void;
+    computed: {
+        totalBears: number;
+    };
 }
 
-export const useBearStore = create<BearState>()((set) => ({
+export const useBearStore = create<BearState>()((set, get) => ({
     blackBears: 10,
     polarBears: 5,
     pandaBears: 1,
 
+    bears: [
+        {
+            id: 1,
+            name: "Oso #1",
+        },
+    ],
+
     increaseBlackBears: (by) => set((state) => ({ blackBears: state.blackBears + by })),
     increasePolarBears: (by) => set((state) => ({ polarBears: state.polarBears + by })),
     increasePandaBears: (by) => set((state) => ({ pandaBears: state.pandaBears + by })),
+    doNothing: () => set((state) => ({ bears: [...state.bears] })),
+    addBear: () =>
+        set((state) => ({ bears: [...state.bears, { id: state.bears.length, name: ` Oso ${state.bears.length} ` }] })),
+    clearBears: () => set({ bears: [] }),
+    computed: {
+        get totalBears() {
+            return get().blackBears + get().polarBears + get().pandaBears + get().bears.length;
+        },
+    },
 }));
