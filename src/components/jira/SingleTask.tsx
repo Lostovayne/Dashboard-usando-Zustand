866dd19cc44cfe368d4eb76c1f7062370a8dfc27
LostoVayne <moises.barrera.devmail.com>
2024-03-10 04:17:58 -0300

    agregado el drag and drop

--- src/components/jira/SingleTask.tsx
+++ src/components/jira/SingleTask.tsx
@@ -13,7 +13,12 @@ interface Props {
  */
 const SingleTask = ({ task }: Props): JSX.Element => {
   return (
-    <div className="flex justify-between items-center mt-5 p-2">
+    <div
+      draggable
+      onDragStart={() => console.log("onDragStart", task.id)}
+      onDragEnd={() => console.log("onDragEnd")}
+      className="flex justify-between items-center mt-5 p-2"
+    >
       <div className="flex justify-center items-center gap-2">
         <p className="font-bold text-base text-navy-700">{task.title}</p>
       </div>
